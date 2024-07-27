ed819eee46d8848f59ffa379b21eeb4efb922904
damiano <macmacroll@gmail.com>
2024-07-27 15:01:11 +0200

    Cleaned up pom.xml

--- src/main/java/com/blogging/dataprovider/CacheDemoApplication.java
+++ src/main/java/com/blogging/dataprovider/CacheDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataProviderApplication {
+public class CacheDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataProviderApplication.class, args);
+		SpringApplication.run(CacheDemoApplication.class, args);
 	}
 
 }
