a771501ee547a205e269bff948443bba97323223
damiano <macmacroll@gmail.com>
2024-07-27 12:22:13 +0200

    Refactored CacheConfig and implemented CustomRedisCacheWriter

--- src/main/java/com/blogging/dataprovider/cache/CacheConfig.java
+++ src/main/java/com/blogging/dataprovider/cache/CacheConfig.java
@@ -1,5 +1,7 @@
 package com.blogging.dataprovider.cache;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
@@ -11,7 +13,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 
 import java.time.Duration;
 
@@ -23,13 +24,32 @@ public class CacheConfig {
     public RedisCacheManager cacheManager(final RedisConnectionFactory connectionFactory,
                                           final RedisTemplate<String, Object> redisTemplate,
                                           final ObjectMapper objectMapper) {
-        CustomRedisCacheWriter cacheWriter = new CustomRedisCacheWriter(connectionFactory,
-                redisTemplate, new GenericJackson2JsonRedisSerializer(objectMapper));
+        var serializationMapper = createSerializationMapper(objectMapper);
+
+        CustomRedisCacheWriter cacheWriter = createCustomCacheWriter(connectionFactory, redisTemplate, serializationMapper);
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer(serializationMapper)))
                 .entryTtl(Duration.ofMinutes(1));
-        return RedisCacheManager.builder(cacheWriter).cacheDefaults(config).build();
+        return RedisCacheManager.builder(cacheWriter)
+                .cacheDefaults(config)
+                .build();
+    }
+
+    private static CustomRedisCacheWriter createCustomCacheWriter(final RedisConnectionFactory connectionFactory,
+                                                                  final RedisTemplate<String, Object> redisTemplate,
+                                                                  final ObjectMapper serializationMapper) {
+        return new CustomRedisCacheWriter(connectionFactory,
+                redisTemplate, new GenericJackson2JsonRedisSerializer(serializationMapper));
+    }
+
+    private static ObjectMapper createSerializationMapper(final ObjectMapper objectMapper) {
+        var serializationMapper = objectMapper.copy();
+        serializationMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        serializationMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.EVERYTHING,
+                JsonTypeInfo.As.PROPERTY);
+        return serializationMapper;
     }
 
     @Bean("customKeyGenerator")
@@ -38,7 +58,7 @@ public class CacheConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(final RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.afterPropertiesSet();
